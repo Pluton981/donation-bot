10e34242be0785657024d2e353bcad65fbdec75a
Pluton981 <157219748+Pluton981@users.noreply.github.com>
2025-04-07 03:35:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -156,3 +156,7 @@ scheduler.start()
 # Обработчики
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CallbackQueryHandler(handle_button))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
